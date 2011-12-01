7781e737187d514a38a9bf65d6af43b71d622933
Michael Enger <mike@thelonelycoder.com>
2011-12-01 20:18:53 +0100

    Basic implementation of the FlickrStream object

--- js/flickrstream.js
+++ js/flickrstream.js
@@ -11,30 +11,131 @@
 	 * @param searchTerm (optional) Search term to start the stream
 	 **/
 	var FlickrStream = function (apiKey, searchTerm) {
+		// Options
+		this.options = {
+			// Flickr API key
+			apiKey: apiKey,
+
+			// Amount of images to get per page
+			perPage: 10,
+
+			// The search term
+			searchTerm: searchTerm
+		};
+
+		// The current page of photos
+		this.page = 0;
+
+		// List of images
+		this.images = [];
+
+		if (searchTerm)
+			this.setSearchTerm(searchTerm);
 	};
 
 	FlickrStream.prototype = {
 
 		/**
-		 * Get the next image in the stream.
+		 * Get the next image in the stream, incrementing the stream.
 		 *
-		 * @param callback The callback which will receive the image object.
+		 * @param callback (optional) Callback which will receive the image object
 		 **/
-		getImage: function (callback) {
+		next: function (callback) {
+			var self = this;
+
+			if (this.images.length !== 0) {
+				// Get the next images
+				if (typeof callback != 'undefined')
+					callback( this.images.shift() );
+			} else {
+				// Get the next batch of images
+				this.page++;
+				this.search(function () {
+					if (callback)
+						self.next(callback);
+				});
+			}
 		},
 
 		/**
 		 * Reset the stream.
 		 **/
 		resetStream: function () {
+			this.page = 0;
+			this.images = [];
+		},
+
+		/**
+		 * Search for images.
+		 *
+		 * @param callback (optional) Callback which should receive the image objects
+		 **/
+		search: function(callback) {
+			var self = this;
+
+			if (!this.searchTerm) {
+				console.error('FlickrStream::search: no search term defined');
+				if (callback)
+					callback();
+				return;
+			}
+
+			// Call flickr
+			$.ajax({
+				url: 'http://api.flickr.com/services/rest/',
+				data: {
+					api_key: this.options.apiKey,
+					format: 'json',
+					method: 'flickr.photos.search',
+					text: this.options.searchTerm,
+					per_page: this.options.perPage,
+					sort: 'relevance'
+				},
+				crossDomain: true,
+				dataType: 'jsonp',
+				jsonp: 'jsoncallback',
+				success: function(json) {
+					// Loop around if we've reached the end
+					if (json.photos.photo.length === 0) {
+						self.resetStream();
+						self.search(callback);
+						return;
+					}
+
+					// Fill 'er up
+					for(var i in json.photos.photo) {
+						var entry = json.photos.photo[i];
+						var image = {
+							id: entry.id,
+							title: entry.title,
+							owner: entry.owner,
+							url: {
+								'default': 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '.jpg',
+								thumbnail: 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '_t.jpg',
+								large: 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '_b.jpg',
+							}
+						}
+
+						self.images.push(image);
+					}
+
+					if (callback)
+						callback();
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.error('AJAX failure "' + textStatus + '" @ FlickrStream::search: ' + errorThrown);
+				}
+			});
 		},
 
 		/**
 		 * Set the search term and reset the stream.
 		 *
-		 * @param searchTerm The search term.
+		 * @param searchTerm The search term
 		 **/
 		setSearchTerm: function (searchTerm) {
+			this.searchTerm = searchTerm;
+			this.resetStream();
 		}
 
 	}
--- js/init.js
+++ js/init.js
@@ -6,11 +6,14 @@ sp = getSpotifyApi(1);
 
 // Create a FlickrStream object
 flickrstream = sp.require('js/flickrstream');
-flickr = new flickrstream.FlickrStream('@todo');
-
+flickr = new flickrstream.FlickrStream('525b48b1850237c4010808f667523170', 'Rise to Remain'); // please don't abuse my API key >_>
 
 // Onload
 $(function() {
+
+	flickr.next(function(image) {
+		console.log(image);
+	});
 
 	// Catch playerStateChanged events
 	sp.trackPlayer.addEventListener("playerStateChanged", function (event) {
@@ -19,3 +22,4 @@ $(function() {
 		}
 	});
 });
+
