a7220276279a389ed3bf0a21bb2eaff6a8e24bdd
Michael Enger <mike@thelonelycoder.com>
2011-12-01 21:31:35 +0100

    Create image slideshow

--- js/init.js
+++ js/init.js
@@ -8,9 +8,13 @@ sp = getSpotifyApi(1);
 flickrstream = sp.require('js/flickrstream');
 flickr = new flickrstream.FlickrStream('525b48b1850237c4010808f667523170'); // please don't abuse my API key >_>
 
+// Settings
+intervalLength = 5000;
+
 // Current objects
 var currentArtist = null;
 var currentImage = null;
+var imageStream = null;
 
 // Onload
 $(function() {
@@ -23,25 +27,34 @@ $(function() {
 			update();
 		}
 	});
+
 });
 
 // Update the stream
 function update() {
 
 	// This will be null if nothing is playing.
-    var playerTrackInfo = sp.trackPlayer.getNowPlayingTrack();
+	var playerTrackInfo = sp.trackPlayer.getNowPlayingTrack();
 
 	if (playerTrackInfo == null) {
-        $('#splash').html("Nothing playing!");
-        currentArtist = null;
-    } else if (playerTrackInfo.track.artists[0].name != currentArtist) {
-        currentArtist = playerTrackInfo.track.artists[0].name;
-        flickr.setSearchTerm(currentArtist);
-        $('#splash').html(currentArtist);
-    }
+		$('#splash').html("Play some music to start the slideshow");
+		currentArtist = null;
+		clearInterval(imageStream);
+		imageStream = null;
+	} else if (playerTrackInfo.track.artists[0].name != currentArtist) {
+		currentArtist = playerTrackInfo.track.artists[0].name;
+		flickr.setSearchTerm(currentArtist);
+		if (!imageStream)
+			imageStream = setInterval('nextImage()', intervalLength);
+	}
 
-	/*flickr.next(function(image) {
-		console.log(image);
-	});*/
 }
 
+// Go to thext next image
+function nextImage() {
+	flickr.next(function(image) {
+		currentImage = image.url.default;
+		console.log(currentImage);
+		$('#splash').html('<img src="' + currentImage + '" alt="' + currentArtist + '">');
+	});
+}
