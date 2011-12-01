8eee330862e97e2dbdf4bfdba9e477af1d838bd8
Michael Enger <mike@thelonelycoder.com>
2011-12-01 20:27:34 +0100

    App now reacts to track changes

--- js/init.js
+++ js/init.js
@@ -6,20 +6,42 @@ sp = getSpotifyApi(1);
 
 // Create a FlickrStream object
 flickrstream = sp.require('js/flickrstream');
-flickr = new flickrstream.FlickrStream('525b48b1850237c4010808f667523170', 'Rise to Remain'); // please don't abuse my API key >_>
+flickr = new flickrstream.FlickrStream('525b48b1850237c4010808f667523170'); // please don't abuse my API key >_>
+
+// Current objects
+var currentArtist = null;
+var currentImage = null;
 
 // Onload
 $(function() {
+	update();
 
-	flickr.next(function(image) {
-		console.log(image);
-	});
-
-	// Catch playerStateChanged events
+	// Catch player changed events
 	sp.trackPlayer.addEventListener("playerStateChanged", function (event) {
 		if (event.data.curtrack == true) {
 			// Track has changed
+			update();
 		}
 	});
 });
+
+// Update the stream
+function update() {
+
+	// This will be null if nothing is playing.
+    var playerTrackInfo = sp.trackPlayer.getNowPlayingTrack();
+
+	if (playerTrackInfo == null) {
+        $('#splash').html("Nothing playing!");
+        currentArtist = null;
+    } else if (playerTrackInfo.track.artists[0].name != currentArtist) {
+        currentArtist = playerTrackInfo.track.artists[0].name;
+        flickr.setSearchTerm(currentArtist);
+        $('#splash').html(currentArtist);
+    }
+
+	/*flickr.next(function(image) {
+		console.log(image);
+	});*/
+}
 
