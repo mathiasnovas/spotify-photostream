a2ac96a7e03c70095956ba3097ed950406bc3e46
Michael Enger <mike@thelonelycoder.com>
2013-09-09 14:34:15 +0200

    Rewrite flickrstream to not use jQuery

--- js/flickrstream.js
+++ js/flickrstream.js
@@ -2,13 +2,13 @@
 
 spotify-flickrstream is licensed under the terms of the ZLIB license
 
-Copyright © 2011 The Lonely Coder 
+Copyright © 2011-2013 The Lonely Coder
 
 This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
 
-Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions: 
- - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required. 
- - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software. 
+Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+ - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
  - This notice may not be removed or altered from any source distribution.
 
 **/
@@ -82,14 +82,75 @@ Permission is granted to anyone to use this software for any purpose, including 
 		 * @param callback (optional) Callback which should receive the image objects
 		 **/
 		search: function(callback) {
-			var self = this;
-
 			if (!this.options.searchTerm) {
 				console.error('FlickrStream::search: no search term defined');
 				if (callback)
 					callback();
 				return;
 			}
+
+			var self = this,
+				request = new XMLHttpRequest(),
+				url = 'http://api.flickr.com/services/rest/',
+				params = {
+					api_key: this.options.apiKey,
+					format: 'json',
+					method: 'flickr.photos.search',
+					text: this.options.searchTerm,
+					per_page: this.options.perPage,
+					page: this.page,
+					sort: 'relevance'
+				},
+				query = '';
+			for (var key in params) {
+				query += (query != '' ? '&' : '?') + encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
+			}
+
+
+			request.onreadystatechange = function () {
+				function jsonFlickrApi(json) {
+					return json;
+				};
+
+				if (this.readyState == 4 && this.status == 200 ) {
+					var json = eval(this.responseText);
+
+					// Loop around if we've reached the end
+					if (json.photos.photo.length === 0) {
+						self.resetStream();
+						self.search(callback);
+						return;
+					}
+
+					// Fill 'er up
+					for(var i in json.photos.photo) {
+						var entry = json.photos.photo[i];
+						var image = {
+							id: entry.id,
+							title: entry.title,
+							owner: entry.owner,
+							link: 'http://www.flickr.com/photos/' + entry.owner + '/' + entry.id,
+							url: {
+								'default': 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '.jpg',
+								thumbnail: 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '_t.jpg',
+								large: 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '_b.jpg',
+							}
+						}
+
+						self.images.push(image);
+					}
+
+					if (callback)
+						callback();
+				}
+			};
+			request.addEventListener("error", function() {
+				console.error(this.responseText);
+			}, false);
+			request.open('GET', url + query, true);
+			request.send();
+
+/*
 
 			// Call flickr
 			$.ajax({
@@ -139,6 +200,7 @@ Permission is granted to anyone to use this software for any purpose, including 
 					console.error('AJAX failure "' + textStatus + '" @ FlickrStream::search: ' + errorThrown);
 				}
 			});
+*/
 		},
 
 		/**
