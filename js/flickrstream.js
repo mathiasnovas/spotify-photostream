30462a79bdf50cb51339b695667e04714c39bd4b
Michael Enger <mike@thelonelycoder.com>
2013-10-19 17:57:21 +0200

    Remove commented-out section

--- js/flickrstream.js
+++ js/flickrstream.js
@@ -149,58 +149,6 @@ Permission is granted to anyone to use this software for any purpose, including 
 			}, false);
 			request.open('GET', url + query, true);
 			request.send();
-
-/*
-
-			// Call flickr
-			$.ajax({
-				url: 'http://api.flickr.com/services/rest/',
-				data: {
-					api_key: this.options.apiKey,
-					format: 'json',
-					method: 'flickr.photos.search',
-					text: this.options.searchTerm,
-					per_page: this.options.perPage,
-					page: this.page,
-					sort: 'relevance'
-				},
-				crossDomain: true,
-				dataType: 'jsonp',
-				jsonp: 'jsoncallback',
-				success: function(json) {
-					// Loop around if we've reached the end
-					if (json.photos.photo.length === 0) {
-						self.resetStream();
-						self.search(callback);
-						return;
-					}
-
-					// Fill 'er up
-					for(var i in json.photos.photo) {
-						var entry = json.photos.photo[i];
-						var image = {
-							id: entry.id,
-							title: entry.title,
-							owner: entry.owner,
-							link: 'http://www.flickr.com/photos/' + entry.owner + '/' + entry.id,
-							url: {
-								'default': 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '.jpg',
-								thumbnail: 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '_t.jpg',
-								large: 'http://farm' + entry.farm + '.staticflickr.com/' + entry.server + '/' + entry.id + '_' + entry.secret + '_b.jpg',
-							}
-						}
-
-						self.images.push(image);
-					}
-
-					if (callback)
-						callback();
-				},
-				error: function(jqXHR, textStatus, errorThrown) {
-					console.error('AJAX failure "' + textStatus + '" @ FlickrStream::search: ' + errorThrown);
-				}
-			});
-*/
 		},
 
 		/**
