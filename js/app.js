23ffee21ae31072608204973c36de3952ce5cf20
Michael Enger <michaelenger@live.com>
2014-08-11 20:57:59 +0200

    Slight structural rewrite of app.js file

--- js/app.js
+++ js/app.js
@@ -1,111 +1,150 @@
 (function() {
 	"use strict";
 
-	// Includes
 	var sp = getSpotifyApi(1),
 		lastfmstream = sp.require("js/lastfmstream");
 
-	// The app itself
 	var app = {
 
-		// Last.fm stream
-		lastfm: new lastfmstream.LastFMStream("dd1d56c289c3bb533e1b4371fb99bd32"), // please don't abuse my API key ^^
-
-		// Settings
-		intervalLength: 7000,
-
-		// Current objects
-		currentArtist: null,
-		currentImage: null,
-		imageStream: null,
+		currentArtist: "",
+		elements: {},
+		images: [],
+		intervalLength: 30000,
+		nextImageTimeout: undefined,
+		sources: {
+			lastfm: new lastfmstream.LastFMStream("dd1d56c289c3bb533e1b4371fb99bd32"), // please don't abuse my API key ^^
+		},
 
 		/**
-		 * Start the application.
+		 * Get the currently playing track.
 		 */
-		init: function() {
-			app.update();
+		getCurrentTrack: function () {
+			var playerTrackInfo = sp.trackPlayer.getNowPlayingTrack();
+
+			if (playerTrackInfo != null && playerTrackInfo.track.artists[0].name) {
+				this.updateArtist(playerTrackInfo.track.artists[0].name);
+			} else {
+				this.updateArtist("");
+			}
+		},
+
+		/**
+		 * Initialize the application.
+		 */
+		init: function () {
+			// DOM elements
+			this.elements.images = document.getElementById("image");
+			this.elements.artist = document.getElementById("artist");
 
 			// Catch player changed events
 			sp.trackPlayer.addEventListener("playerStateChanged", function (event) {
-				if (event.data.curtrack == true) {
-					// Track has changed
-					app.update();
-				}
-			});
+				this.getCurrentTrack();
+			}.bind(this));
 
+			this.getCurrentTrack();
 		},
 
-		// Go to thext next image
-		nextImage: function() {
-			app.lastfm.next(function(image) {
-				var img = new Image();
-
-				// Load new image
-				app.currentImage = image.url;
-
-				img.onload = function() {
-					var imageDiv = document.getElementById("image"),
-						existingImages = document.querySelectorAll("#image > div"),
-						newImage = document.createElement("div");
-
-					if (existingImages.length) {
-						for (var i = 0; i < existingImages.length; i++) {
-							existingImages[i].style.opacity = 0;
-						}
-						setTimeout(function() {
-							for (var i = 0; i < existingImages.length; i++) {
-								existingImages[i].remove();
-							}
-						}, 1000);
-					}
-
-					// Create new image div
-					newImage.id = image.id;
-					newImage.style.backgroundImage = "url('" + app.currentImage + "')";
-					imageDiv.appendChild(newImage);
-
-					// Show new image
-					newImage.style.opacity = 1;
-
-					// Prepare next image
-					app.imageStream = setTimeout(function() {
-						app.nextImage();
-					}, app.intervalLength);
-				}
-				img.src = app.currentImage;
-
-			});
+		/**
+		 * Load the images for the current artist.
+		 */
+		loadImages: function () {
+			this.sources.lastfm.setSearchTerm(this.currentArtist);
+			this.sources.lastfm.next(function (image) {
+				this.images.push(image);
+				this.nextImage(); // @todo: only do this when all sources are done
+			}.bind(this));
 		},
 
-		// Update the stream
-		update: function() {
+		/**
+		 * Show the next image.
+		 */
+		nextImage: function () {
+			var currentImage,
+				image,
+				index;
 
-			// This will be null if nothing is playing.
-			var playerTrackInfo = sp.trackPlayer.getNowPlayingTrack();
-
-			if (playerTrackInfo == null) {
-				app.pause();
-				app.currentArtist = null;
-				app.currentImage = null;
-				document.getElementById("artist").innerHTML = "Waiting for music...";
-				document.getElementById("image").style.display = "none";
-			} else if (playerTrackInfo.track.artists[0].name != app.currentArtist) {
-				app.currentArtist = playerTrackInfo.track.artists[0].name;
-				app.lastfm.setSearchTerm(app.currentArtist);
-				if (!app.imageStream) {
-					app.nextImage();
-				}
-				document.getElementById("artist").innerHTML = app.currentArtist;
-				document.getElementById("image").style.display = "block";
+			if (this.nextImageTimeout) {
+				clearTimeout(this.nextImageTimeout);
+				this.nextImageTimeout = undefined;
 			}
 
+			console.log("Next image");
+
+			if (!this.images.length) {
+				console.warn("No images to display");
+				return;
+			}
+
+			index = Math.floor(Math.random() * (this.images.length));
+			currentImage = this.images[index];
+			image = new Image();
+
+			image.onload = function() {
+				var imageContainer = document.createElement("div");
+
+				this.removeImages();
+
+				imageContainer.id = currentImage.id;
+				imageContainer.style.backgroundImage = "url('" + currentImage.url + "')"
+				this.elements.images.appendChild(imageContainer);
+
+				setTimeout(function () {
+					imageContainer.className = "current";
+				}, 100); // allow for the fade effect
+
+				this.nextImageTimeout = setTimeout(function() {
+					app.nextImage();
+				}, app.intervalLength);
+			}.bind(this);
+
+			image.src = currentImage.url;
+		},
+
+		/**
+		 * Remove existing images.
+		 */
+		removeImages: function () {
+			var existingImages = this.elements.images.querySelectorAll("#image > div");
+
+			if (existingImages.length) {
+				for (var i = 0; i < existingImages.length; i++) {
+					existingImages[i].className = "";
+				}
+				setTimeout(function() {
+					for (var i = 0; i < existingImages.length; i++) {
+						existingImages[i].remove();
+					}
+				}, 1000);
+			}
+		},
+
+		/**
+		 * Update the currently playing artist.
+		 *
+		 * @param string artist New artist to use
+		 */
+		updateArtist: function (artist) {
+			if (this.currentArtist == artist) {
+				return;
+			}
+
+			this.currentArtist = artist;
+			this.images = [];
+
+			if (artist != "") {
+				console.log("Update artist: " + artist);
+
+				this.elements.artist.innerHTML = artist;
+				this.loadImages();
+			} else {
+				this.elements.artist.innerHTML = "Waiting for music...";
+			}
 		}
 
 	};
 
-	window.onload = function() {
-		// Let"s do this!
+	window.onload = function () {
 		app.init();
-	}
+	};
 
 })();
